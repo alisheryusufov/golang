9fe082cc45e3445879a1e14ee65623a65705bc35
alisheryusufov <ail>
2025-08-17 18:10:16 +0800

    basics

--- basics/main.go
+++ basics/main.go
@@ -4,21 +4,156 @@ import (
 	"fmt"
 )
 
+// "math/rand"
+// "time"
+
 func main() {
 
-	score := 85
+	message := "Hello World"
 
-	if score >= 90 {
-		fmt.Println("Grade: A")
-	} else if score >= 80 {
-		fmt.Println("Grade: B")
-	} else if score >= 70 {
-		fmt.Println("Grade: C")
-	} else {
-		fmt.Println("Grade: D")
+	for i, v := range message {
+		fmt.Println(i, v)
 	}
 
 }
+
+// myMap := make(map[string]int)
+
+// myMap["student"] = 5
+// myMap["alice"] = 2
+
+// v, s := myMap["alice"]
+// // delete(myMap)
+// fmt.Println(v, s)
+
+// studentsScore := map[string]int{
+// 	"alice": 90,
+// 	"jame":  87,
+// 	"jake":  80,
+// }
+
+// fmt.Println(studentsScore)
+
+// maps.Equal()
+
+// a := [5]int{1, 2, 3, 4, 5}
+
+// s := a[1:4]
+
+// fmt.Println(s, cap(s))
+
+// s = append(s, 2, 4, 5)
+
+// fmt.Println(s, cap(s))
+
+// sliceCopy := make([]int, len(s))
+// copy(sliceCopy, s)
+
+// fmt.Println(s, sliceCopy)
+
+// for i, v := range s {
+// 	fmt.Println(i, v)
+// }
+
+// fmt.Println(slices.Equal(s, sliceCopy))
+
+// originalArray := [3]int{1, 2, 3}
+
+// var coppiedArray *[3]int
+
+// coppiedArray = &originalArray
+
+// coppiedArray[0] = 100
+
+// fmt.Println(originalArray, *coppiedArray)
+
+// var numbers [5]int
+
+// numbers[len(numbers)-1] = 20
+// fmt.Println(numbers)
+
+// numbers[0] = 10
+// fmt.Println(numbers)
+
+// for index, value := range numbers {
+// 	fmt.Printf("Index: %d, Value: %d\n", index, value)
+// }
+
+// for i := 0; i < len(numbers); i++ {
+// 	fmt.Println("Index:", i, ":", numbers[i])
+// }
+
+// fruits := [4]string{"apple", "banana", "chery", "orange"}
+// fmt.Println("Fruits array:", fruits)
+
+// originalArray := [5]int{1, 2, 3, 4, 5}
+
+// copiedArray := originalArray
+
+// copiedArray[0] = 10
+// fmt.Println("Original array:", originalArray)
+// fmt.Println("Copied array:", copiedArray)
+
+// fruit := "apple"
+
+// switch fruit {
+// case "apple":
+// 	fmt.Println("You selected an apple.")
+// case "banana":
+// 	fmt.Println("You selected a banana.")
+// default:
+// 	fmt.Println("Unknown fruit selected.")
+// }
+
+// day := "Monday"
+
+// switch day {
+// case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
+// 	fmt.Println("It's a weekday.")
+// case "Saturday", "Sunday":
+// 	fmt.Println("It's a weekend.")
+// default:
+// 	fmt.Println("Unknown day selected.")
+// }
+
+// number := 15
+
+// switch {
+// case number < 10:
+// 	fmt.Println("The number is less than 10.")
+// case number >= 10 && number < 20:
+// 	fmt.Println("The number is between 10 and 20.")
+// case number >= 20:
+// 	fmt.Println("The number is 20 or greater.")
+// default:
+// 	fmt.Println("Unknown number range.")
+// }
+
+// num := 2
+
+// switch {
+// case num > 1:
+// 	fmt.Println("The number is greater than 1.")
+// 	fallthrough
+// case num == 2:
+// 	fmt.Println("The number is equal to 2.")
+// case num < 1:
+// 	fmt.Println("The number is less than 1.")
+// default:
+// 	fmt.Println("The number is unknown.")
+// }
+
+// score := 85
+
+// if score >= 90 {
+// 	fmt.Println("Grade: A")
+// } else if score >= 80 {
+// 	fmt.Println("Grade: B")
+// } else if score >= 70 {
+// 	fmt.Println("Grade: C")
+// } else {
+// 	fmt.Println("Grade: D")
+// }
 
 // source := rand.NewSource(time.Now().UnixNano())
 // random := rand.New(source)
